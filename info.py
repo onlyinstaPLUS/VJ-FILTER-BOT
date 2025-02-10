4f4b93cc15892de7efb561d2ef4c6fd02819940d
onlyinstaPLUS <134713123+onlyinstaPLUS@users.noreply.github.com>
2025-02-10 15:35:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,10 +88,10 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/vj_bot_disscussion')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/vj_botz')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'vj_bot_disscussion') # Support Chat Link Without https:// or @
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/kingvj01')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/EarnMoneyTips_Official')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/EarnMoneyTips_Official')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/EarnMoneyTips_Official') # Support Chat Link Without https:// or @
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/HackerPushkar')
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
