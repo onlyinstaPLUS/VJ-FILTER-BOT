68cddd1107b342622097869b761f26ee33252773
onlyinstaPLUS <134713123+onlyinstaPLUS@users.noreply.github.com>
2025-04-25 20:19:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002428763325'))
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002089257903').split()]  # For Multiple Id Use One Space Between Each.
 
 #Extra Backup Channel
-Backup_Channel = environ.get('BACKUP_CHANNEL', '-1001860294823') # give your force subscribe channel id here else leave it blank
+Backup_Channel = environ.get('BACKUP_CHANNEL', '-1002348853799') # give your force subscribe channel id here else leave it blank
 BACKUP_CHANNEL = int(Backup_Channel) if Backup_Channel and id_pattern.search(Backup_Channel) else None
 
 # auth_channel means force subscribe channel.
